d04f5582e4725e06657a308d332ae236bfd5a00c
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-07-23 02:15:30 -0700

    initial upload and implementing app.css for better representing the api

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import PokemonList from './PokemonList';
+import TypeList from './TypeList';
+import PokemonDetail from './PokemonDetail';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      offset: 0,
+      pokemons: []
+    }
+
+    this.handlePokemonListResponse = this.handlePokemonListResponse.bind(this);
+    this.handlePokemonDetailsResponse = this.handlePokemonDetailsResponse.bind(this);
+    this.handleGetPrev20 = this.handleGetPrev20.bind(this);
+    this.handleGetNext20 = this.handleGetNext20.bind(this);
+    this.fetchPokemons = this.fetchPokemons.bind(this);
+  }
+
+  toPokemon(response) {
+    return {
+      id: response.data.id,
+      name: response.data.name,
+      image_url: response.data.sprites.front_default,
+    };
+  }
+
+  handlePokemonDetailsResponse(responses) {
+    this.setState({
+      loaded: true,
+      pokemons: responses.map(this.toPokemon)
+    });
+  }
+
+  handlePokemonListResponse(response) {
+    Promise.all(response.data.results.map((pokemon) => axios.get(pokemon.url)))
+      .then(this.handlePokemonDetailsResponse)
+  }
+
+  componentDidMount() {
+    this.fetchPokemons(0);
+  }
+
+  handleGetNext20() {
+    const offset = this.state.offset + 20;
+    this.fetchPokemons(offset);
+    this.setState({offset});
+  }
+
+  handleGetPrev20() {
+    const offset = this.state.offset - 20;
+    this.fetchPokemons(offset);
+    this.setState({offset});
+  }
+
+  fetchPokemons(offset) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}`).then(this.handlePokemonListResponse)
+  }
+
+  render() {
+    if (this.state.loaded) {
+      return (
+        <Router>
+          <nav id="nav-bar">
+                <Link id="pokemon-list-btn" to="/pokemons">Pokemons</Link><Link id="type-list-btn" to="/types">Types</Link>
+          </nav>
+          <Switch>
+            <Redirect exact from="/" to="/pokemons" />
+            <Route exact path="/pokemons">
+              <PokemonList offset={this.state.offset}
+                           pokemons={this.state.pokemons}
+                           getPrev20={this.handleGetPrev20}
+                           getNext20={this.handleGetNext20}/>
+            </Route>
+            <Route exact path="/types">
+              <TypeList />
+            </Route>
+            <Route path="/pokemons/:id" children={<PokemonDetail pokemons={this.state.pokemons}/>} />
+          </Switch>
+        </Router>
+      );
+    } else {
+      return <div class="loader"></div>
+    }
+  }
+}
+
+export default App;
---
+++ src/PokemonList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class PokemonList extends React.Component {
+  render() {
+    const prevButton = this.props.offset === 0
+      ? <React.Fragment/>
+      : <button onClick={this.props.getPrev20}>Prev 20</button>;
+    return (
+      <React.Fragment>
+        <h1>Pokemons</h1>
+        <ul>
+          {this.props.pokemons.map(this.mapPokemonToListItem)}
+        </ul>
+        {prevButton}
+        <button onClick={this.props.getNext20}>Next 20</button>
+      </React.Fragment>
+    );
+  }
+
+  mapPokemonToListItem(pokemon) {
+    return (
+      <li key={pokemon.url}>
+        <Link to={`/pokemons/${pokemon.id}`}>
+          <img alt="pokemonListItem" src={pokemon.image_url}/>
+          {pokemon.name}
+        </Link>
+      </li>
+    )
+  }
+}
+
+export default PokemonList;
